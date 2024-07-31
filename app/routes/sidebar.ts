6d6f1cdd3bfca1b13ffbed53cbd7a6a08a38f318
agungd3v <agungd3v@gmail.com>
2024-07-31 18:36:44 +0700

    update

--- app/routes/sidebar.ts
+++ app/routes/sidebar.ts
@@ -2,36 +2,36 @@ export const rts = [
   {
     label: "Dashboard",
     path: "/dashboard/admin",
-    role: 1
+    role: "1"
   },
   {
     label: "Product",
     path: "/dashboard/admin/product",
-    role: 1
+    role: "1"
   },
   {
     label: "My Order",
     path: "/dashboard/admin/order",
-    role: 1
+    role: "1"
   },
   {
     label: "Dashboard",
     path: "/dashboard/manager",
-    role: 2
+    role: "2"
   },
   {
     label: "Orders",
     path: "/dashboard/manager/order",
-    role: 2
+    role: "2"
   },
   {
     label: "Dashboard",
     path: "/dashboard/viewer",
-    role: 3
+    role: "3"
   },
   {
     label: "My Order",
     path: "/dashboard/viewer/order",
-    role: 3
+    role: "3"
   }
 ];
